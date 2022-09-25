0d6ee8dce3e520530dc63414b947b73da585ecb2
mwinkelhorst <mwinkelhorst@gmail.com>
2022-09-25 09:27:07 +0200

    First commit of training website

---
+++ MyFirstWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Models;
+using MyFirstWebApp.Services;
+
+namespace MyFirstWebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] [FromBody]
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(string productId, int rating)
+        {
+            ProductService.AddRating(productId, rating);
+            return Ok();
+        }
+    }
+}
---
+++ MyFirstWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyFirstWebApp.Models;
+using MyFirstWebApp.Services;
+
+namespace MyFirstWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product>? Products { get; private set; }
+
+        public void OnGet() => Products = ProductService.GetProducts();
+    }
+}
